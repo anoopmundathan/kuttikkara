fe4b21c9efaceed2cb3a1e35eb8acfc4610a64f5
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-03 09:05:50 +0100

    Initial commit :tada:

---
+++ server/Server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+function Server(port = process.env.PORT || 3003) {
+	this._app = express();
+	this._port = port;
+	this._app.use('/', express.static('./public'));
+}
+
+Server.prototype.listen = function() {
+	this._app.listen(this._port, () => {
+		console.log(`Server running at port ${this._port}`);
+	});
+};
+
+module.exports = Server;

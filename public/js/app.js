f1f1e31a1207f17ec8790bb55fb5ba4515fe421a
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-03 10:30:22 +0100

    Seperate JS file

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Kuttikara = (function() {
+
+	function animateMenu() {
+		var hamburger = document.querySelector('.header-hamburger-icon');
+		var slideMenu = document.querySelector('.slidemenu');
+		var container = document.querySelector('#main-container');
+		var toggle = false;
+		
+		hamburger.addEventListener('click', function() {
+			toggle = !toggle;
+			
+			if (toggle) {
+				slideMenu.style.transform = ("translate(0,0)");
+				container.style.transform = ("translate(250px, 0)");
+			} else {
+				slideMenu.style.transform = ("translate(-200%,0)");
+				container.style.transform = ("translate(0, 0)");
+			}
+		});	
+	}
+	return {
+		animateMenu: animateMenu
+	}
+})();
+
+Kuttikara.animateMenu();
--- server/Server.js
+++ server/Server.js
@@ -1,3 +1,4 @@
+'use strict'
 const express = require('express');
 
 function Server(port = process.env.PORT || 3003) {

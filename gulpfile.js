2b730fc9a6d76d9aa7cdc5289d9d8c75b313625f
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-03 16:02:34 +0100

    Add pug template

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,7 @@
 'use strict';
 const gulp = require('gulp');
-const htmlmin = require('gulp-htmlmin');
 const cssmin = require('gulp-cssmin');
 const uglify = require('gulp-uglify');
-
-gulp.task('minify-html', () => {
-	return gulp.src('public/index.html')
-		.pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(gulp.dest('build/public'));
-});
 
 gulp.task('minify-css', () => {
 	return gulp.src('public/css/*.css')
@@ -22,10 +15,9 @@ gulp.task('uglify', () => {
 		.pipe(gulp.dest('build/public/js'));
 });
 
-gulp.task('default', ['minify-html', 'minify-css', 'uglify']);
+gulp.task('default', ['minify-css', 'uglify']);
 
 gulp.task('watch', () => {
 	gulp.watch('public/css/*.css', ['minify-css']);
 	gulp.watch('public/js/*.js',['uglify']);
-	gulp.watch('public/index.html',['minify-html']);
 });
--- server/Server.js
+++ server/Server.js
@@ -1,11 +1,20 @@
 'use strict'
 const express = require('express');
+const path = require('path');
 
 function Server(port = process.env.PORT || 3003) {
 	this._app = express();
 	this._port = port;
 
 	this._app.use('/', express.static('./build/public'));
+	
+	this._app.get('/', (req, res) => {
+		res.render('layout');
+	});
+
+	// View engine setup
+	this._app.set('views', path.join(__dirname, '..', 'views'));
+	this._app.set('view engine', 'pug');
 }
 
 Server.prototype.listen = function() {
